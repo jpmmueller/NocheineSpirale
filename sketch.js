e84b07690d3fffb61d48fa2e7b83f8203ac304a0
jpmmueller <jpmmueller@gmail.com>
2022-02-22 20:10:39 +0100

    Das Grundgerüst

--- sketch.js
+++ sketch.js
@@ -1,7 +1,50 @@
+let mouseIsOn = false;
+let x,y;
+let px,py;
+let stepSize;
+let num = 1;
+let step = 1;
+let state = 1;
+let turnCounter = 0;
+
 function setup() {
-  createCanvas(400, 400);
-}
+  createCanvas(500, 500);
+  background(0);
+  x = width / 2;
+  y = height / 2;
+  stepSize = 70;
+
+}// setup() Ende ---
 
 function draw() {
-  background(220);
+  if (mouseIsOn){
+    textAlign(CENTER,CENTER);
+    noStroke();
+    textSize(45);
+    fill(255);
+    text(num, x, y);
+    if (num > 1){
+      push();
+      stroke(255);
+      strokeWeight(1);
+      line(px,py,x-(stepSize/3),y);
+      px = x+(stepSize/3);
+      py = y;
+      pop();
+    }
+    theMagic();
+  }// --- mouseIsOn() Ende ---
+  mouseIsOn = false;
+}// draw() Ende ---
+
+function mousePressed(){
+  mouseIsOn = true;
+}// mousePressed() Ende ---
+
+function theMagic(){
+
+  x += stepSize;
+  num++;
+  step++;
 }
+
