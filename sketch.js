d06d65aed25c05c8db3bef8968e637ab506743c0
jpmmueller <jpmmueller@gmail.com>
2022-02-23 13:32:02 +0100

    oh diese linien...

--- sketch.js
+++ sketch.js
@@ -1,40 +1,52 @@
 let mouseIsOn = false;
 let x,y;
+let ax,ay;
 let px,py;
 let stepSize;
 let num = 1;
 let step = 1;
 let state = 1;
 let turnCounter = 0;
+let fontSize;
+let lineOffset;
+let maxSteps;
 
 function setup() {
   createCanvas(500, 500);
   background(0);
+  stepSize = 50;
+  const rows = width / stepSize;
+  const cols = height / stepSize;
+  maxSteps = rows * cols;
+  fontSize = 45;
   x = width / 2;
   y = height / 2;
-  stepSize = 70;
-
+  lineOffset = fontSize - 20;
+  px = x + (lineOffset);
+  py = y;
 }// setup() Ende ---
 
 function draw() {
-  if (mouseIsOn){
+  if (mouseIsOn ){
     textAlign(CENTER,CENTER);
     noStroke();
-    textSize(45);
+    textSize(fontSize);
     fill(255);
     text(num, x, y);
     if (num > 1){
       push();
       stroke(255);
       strokeWeight(1);
-      line(px,py,x-(stepSize/3),y);
-      px = x+(stepSize/3);
-      py = y;
+      line(px,py,ax,ay);      
       pop();
     }
     theMagic();
   }// --- mouseIsOn() Ende ---
   mouseIsOn = false;
+
+  if(step > maxSteps){
+    noLoop();
+  }
 }// draw() Ende ---
 
 function mousePressed(){
@@ -42,9 +54,18 @@ function mousePressed(){
 }// mousePressed() Ende ---
 
 function theMagic(){
+  if (step % 2 == 0){
+    y -= stepSize;
+    py = ay + (lineOffset); 
+    ay = y - (lineOffset);
+  }else{
+    x += stepSize;
+    px = ax - (lineOffset);
+    ax = x + (lineOffset);
+  }
 
-  x += stepSize;
   num++;
   step++;
+
 }
 
